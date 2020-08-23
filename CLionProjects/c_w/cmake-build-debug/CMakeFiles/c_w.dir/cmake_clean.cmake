fac7eabd8722523d68ad3d0860208cd60f0fbdcd
Lharys Noisome <lnoisome@il-k3.msk.21-school.ru>
2020-08-23 13:58:46 +0300

    add2

---
+++ CLionProjects/c_w/cmake-build-debug/CMakeFiles/c_w.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_w.dir/main.c.o"
+  "c_w"
+  "c_w.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_w.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
